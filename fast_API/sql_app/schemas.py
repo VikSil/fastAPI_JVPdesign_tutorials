8835c462ba397cda717834e5973f8a81b4c1b8ee
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-10-30 22:15:51 +0000

    Add examples of database operations

---
+++ fast_API/sql_app/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    # docs on model config here:
+    # https://docs.pydantic.dev/1.10/usage/model_config/
+    class Config:
+        orm_mode = True  # retrieve related items with all instances
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
